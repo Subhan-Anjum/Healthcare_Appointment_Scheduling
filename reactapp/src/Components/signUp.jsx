0eefa07252cbdd6fdc16bbe178fa6a04d6b2bdbe
Laiba <115210630+Laiba51-CS@users.noreply.github.com>
2023-11-22 13:17:42 +0500

    Add SignUp feature

--- reactapp/src/Components/signUp.jsx
+++ reactapp/src/Components/signUp.jsx
@@ -1,31 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {Link,useHistory} from 'react-router-dom';
 
-export default function SignIn() {
-    return (
-        <div>
-            <div class="login-box">
-                <h1>Create Account</h1>
-                <form name="signup">
-                    <div class="textbox">
-                        <i class="fas fa-user"></i>
+export default function SignUp() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
+  const history=useHistory();
 
-                        Username
-                        <input type="text" name="userid" />
-                    </div>
+  const handleSignup = async () => {
+    try {
+      const response = await fetch('http://localhost:3005/api/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, email, password,role }),
+      });
 
-                    <div class="textbox">
-                        <i class="fas fa-lock"></i>
-                        Email<input type="email" name="email" />
-                    </div>
-                    <div class="textbox">
-                        <i class="fas fa-lock"></i>
-                        Password<input type="password" name="pswrd" />
-                    </div>
-                    <input type="button" onclick="check(this.form)" value="Signup" />
-                    
-                </form>
+      if (response.ok) {
+        // Signup successful, handle accordingly (e.g., redirect)
+        console.log('Signup successful');
+        history.push("/");
+      } else {
+        // Handle signup failure
+        console.error('Signup failed');
+      }
+    } catch (error) {
+      console.error('Error during signup:', error.message);
+    }
+  };
 
-            </div>
-        </div >
-    )
+  return (
+    <div>
+      <div className="login-box">
+        <h1>Create Account</h1>
+        <form>
+          <div className="textbox">
+            <i className="fas fa-user"></i>
+            Username
+            <input
+              type="text"
+              name="userid"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="textbox">
+            <i className="fas fa-lock"></i>
+            Email
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="textbox">
+            <i className="fas fa-lock"></i>
+            Password
+            <input
+              type="password"
+              name="pswrd"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="textbox">
+            <i className="fas fa-lock"></i>
+            Role
+            <input
+              type="text"
+              name="role"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            />
+          </div>
+          <input type="button" onClick={handleSignup} value="Signup" />
+        </form>
+      </div>
+    </div>
+  );
 }
